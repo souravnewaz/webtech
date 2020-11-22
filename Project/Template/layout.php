6f17a8be26a43acb9aa03ab9fca9b90ca5b4bf8b
souravnewaz <rakibulnewaz@gmail.com>
2020-11-22 21:56:57 +0600

    Add files via upload
    Airlines Project

---
+++ Project/Template/layout.php
@@ -0,0 +1,28 @@
+<style>
+    .container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+   
+}
+
+header {
+  height: 80px;
+  flex-shrink: 0;
+  background: #273746;color:#fff; text-align: center; padding:20px;
+}
+
+footer {
+  height: 80px;
+  flex-shrink: 0;
+  background: #273746;color:#fff; text-align: center;
+  padding:20px;
+}
+.header h2, .footer h2 {margin: 0;}
+
+main {
+  flex: 1 0 auto;
+  background: #ddd;
+   padding: 20px;
+}
+</style>
